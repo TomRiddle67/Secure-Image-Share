d10979dfc19b04996fe5d1f96244aea2bb410b61
TomRiddle67 <courage0047@gmail.com>
2026-04-03 13:29:40 +0100

    Save Image Logic figure out :)

---
+++ storage.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+from config import IMAGES_DIR
+from id_generator import generate_id
+
+def save_image(source_path):
+    os.makedirs(IMAGES_DIR, exist_ok=True)
+    image_id = generate_id()
+    extension = os.path.splitext(source_path)[1]
+    dest_path = os.path.join(IMAGES_DIR, image_id + extension)
+    shutil.copy(source_path, dest_path)
+    return image_id
+    
+
+def get_image(image_id):
