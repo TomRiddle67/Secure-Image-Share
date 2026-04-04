f0f7b08be86010e9876adc487854db4b4651e818
TomRiddle67 <courage0047@gmail.com>
2026-04-04 15:00:48 +0100

    fast api /upload try it out-Server responds with Filename

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get ("/")
 def home ():
     return{"Message": "🛠 Welcome to Secure Image Share API!"}
+
+@app.post ("/upload")
+def upload_image(file: UploadFile = File(...)):
+    return{"Filename": file.filename}
 
